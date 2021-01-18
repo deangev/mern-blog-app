d8ecfb91f3da4efa88900a05e8f640024079aea0
dean <gevadean@gmail.com>
2021-01-18 15:52:37 +0200

    make it better

--- client/src/context/urlProvider.js
+++ client/src/context/urlProvider.js
@@ -1,3 +1,4 @@
-export const url = "http://localhost:5000";
+// export const url = "http://localhost:5000";
 
-// const url = "https://mern-chat-posts.herokuapp.com"
+// export const url = "https://mern-chat-posts.herokuapp.com"
+export const url = "https://mern-blog-cb.herokuapp.com"
